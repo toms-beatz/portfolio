f88beeb55f5f9a318333efbd83e5652bcb24afc9
toms412 <thomas.mazeau@icloud.com>
2024-08-04 15:35:11 +0200

    Test : Vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <MaxWidthWrapper>
         <h1 className="font-title text-5xl font-bold text-green-3">Home</h1>
         <p className="font-body text-xl font-normal text-green-2">Welcome to the home page</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, libero numquam ab unde voluptate aliquam in expedita voluptates cumque qui atque. Eveniet, error. Ratione laborum ipsam doloremque. Ipsam, fugit hic! <span className="text-green-1 underline">blabla</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat suscipit laboriosam officia? Illum consequatur amet ullam voluptate quia facere cum labore necessitatibus magnam vel odit perferendis, ut saepe excepturi hic?</p>
+        <p>Vercel test</p>
         <Button className="bg-purple">Click me</Button>
       </MaxWidthWrapper >
     </>
